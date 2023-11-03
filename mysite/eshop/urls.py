b835f35b38f1fc7d82af6258bdde0ef19e0837ad
Donatas Noreika <donoras@gmail.com>
2023-11-03 13:26:24 +0200

    +Padaryti, kad prisjungusiam vartotojui būtų rodomi jo užsakymai

--- mysite/eshop/urls.py
+++ mysite/eshop/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import ProductListView
+from .views import ProductListView, OrderListView, OrderDetailView
 
 urlpatterns = [
-    path("", ProductListView.as_view(), name="products")
+    path("", ProductListView.as_view(), name="products"),
+    path('orders/', OrderListView.as_view(), name="orders"),
+    path('orders/<int:pk>/', OrderDetailView.as_view(), name="order"),
 ]
--- mysite/eshop/views.py
+++ mysite/eshop/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Product
+from .models import Product, Order
 
 # Create your views here.
 class ProductListView(generic.ListView):
     model = Product
     template_name = "products.html"
     context_object_name = "products"
+
+
+class OrderListView(generic.ListView):
+    model = Order
+    template_name = "orders.html"
+    context_object_name = "orders"
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = "order.html"
+    context_object_name = "order"
+
