d8801a15470f688b44e8dd07e4f173f55772f19e
Donatas Noreika <donoras@gmail.com>
2023-11-03 12:29:19 +0200

    Aprašyti modeliai ir admin.py failas

--- mysite/eshop/admin.py
+++ mysite/eshop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Status, Order, OrderLine
+
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['date', 'date', 'status']
+    inlines = [OrderLineInLine]
+
+admin.site.register(Product)
+admin.site.register(Status)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLine)
--- mysite/eshop/models.py
+++ mysite/eshop/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(verbose_name="Pavadinimas", max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Product(models.Model):
+    name = models.CharField(verbose_name="Pavadinimas", max_length=50)
+    price = models.FloatField(verbose_name="Kaina")
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(to=User, verbose_name="Vartotojas", on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    status = models.ForeignKey(to="Status", verbose_name="Būsena", on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} ({self.date}) - {self.status}"
+
+
+class OrderLine(models.Model):
+    order = models.ForeignKey(to="Order", on_delete=models.CASCADE, related_name="lines")
+    product = models.ForeignKey(to="Product", verbose_name="Produktas", on_delete=models.SET_NULL, null=True,
+                                blank=True)
+    quantity = models.IntegerField(verbose_name="Kiekis")
